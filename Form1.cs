aea68f32c44a4b9bed5ca61b63e364510919c1d9
Ryn <wykoffrachel@gmail.com>
2026-03-03 15:37:38 -0600

    Add files via upload
    This is the Winform for the Hotel Booking Manager project.

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+namespace HotelBookingManager;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void dtOut_ValueChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void lvBookings_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
